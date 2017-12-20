cea9b7e5a8f5dba2d4ab78bee15ab08777e3c4d7
koo <koo@sk.com>
2017-12-20 14:46:00 +0900

    guide 분리 및 기본 스타일 적용

---
+++ guide/script.js
@@ -0,0 +1,20 @@
+
+
+/*
+ * howto 표현하기
+ */
+function function_name() {
+	// body...
+}
+
+/*
+ * Guide LNB
+ */
+function function_name() {
+	// body...
+}
+
+
+$(document).ready( function(){
+	function_name();
+});
