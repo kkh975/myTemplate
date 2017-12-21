940867fc811972e4a178d22398ea046cd3a2cf4c
koo <koo@sk.com>
2017-12-21 11:05:14 +0900

    placeholder 기능 및 guide 일부 스타일

--- guide/script.js
+++ guide/script.js
@@ -10,11 +10,31 @@ function function_name() {
 /*
  * Guide LNB
  */
-function function_name() {
-	// body...
+function guideLnb() {
+	$('.guide_lnb a').on('click', function(e){
+		var $this = $(this)
+
+		if ($this.attr('target') != '_blank') {
+			e.preventDefault();
+
+			$('.guide_window').attr('src', $this.attr('href'));	
+		}
+	})
+}
+
+function guideViewSize() {
+	var $win = $( window );
+	var $body = $('.guide_body');
+
+	$body.height( $win.height() )
+
+	$win.on('resize', function(){
+		$body.height( $win.height() )
+	})
 }
 
 
 $(document).ready( function(){
-	function_name();
+	guideLnb();
+	guideViewSize();
 });
